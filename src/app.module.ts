a38c1b43446f1cbfc00fc1625e1f70087cbfae54
ldb-258 <102220094@sv1.dut.udn.vn>
2025-01-12 18:42:59 +0700

    connect db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { AppController } from '@/app.controller'
+import { AppService } from '@/app.service'
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
+import * as Joi from 'joi'
+import { UsersModule } from './users/users.module'
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      validationOptions: {
+        abortEarly: false,
+      },
+      isGlobal: true,
+      envFilePath: '.env',
+      cache: true,
+      expandVariables: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string(),
+        PORT: Joi.number().default(8080),
+      }),
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@/app.module'
+import { Logger } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 8100);
+  const app = await NestFactory.create(AppModule)
+  await app.listen(process.env.PORT)
+  Logger.log(
+    `Server running on http://localhost:${process.env.PORT}`,
+    'Bootstrap',
+  )
 }
-bootstrap();
+bootstrap()
